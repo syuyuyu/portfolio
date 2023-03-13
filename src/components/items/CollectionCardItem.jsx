1697ec9ee31fc2f243159111886a3ccfca20d8e4
Syuan Liu <syuan.haha@gmail.com>
2023-03-13 11:51:20 +0800

    新增點擊翻轉

--- src/components/items/CollectionCardItem.jsx
+++ src/components/items/CollectionCardItem.jsx
@@ -6,10 +6,12 @@ const CardItemContainer = styled.div`
   width: 100%;
   height: 100%;
   margin-bottom: 5px;
+  /* padding-bottom: 5px; */
   min-width: 100%;
   .hover{
-    /* width: 200%; */
-    /* height: 200%; */
+    /* width: 110%;
+    height: 110%; */
+    /* padding: 5px; */
     border-radius: 10px;
     position: relative;
     margin: auto 0;
@@ -17,22 +19,45 @@ const CardItemContainer = styled.div`
     box-shadow: 0px 0px 10px 0px var(--color-dark60);
     cursor: pointer;
   }
+  .click{
+    transform:rotateY(180deg);
+  }
 `
 
 // const Info =styled.div``
 
 
 const CollectionCardItem = ({url})=>{
+  const [isClick,setIsClick]=useState(false);
   const [isHover,setIsHover]=useState(false);
 
+  //滑鼠hover
   const handleMouseOver=(e)=>{
+    if(isHover){
+      setIsHover(false)
+      e.target.classList.remove('hover')
+      return
+    }
     setIsHover(true)
     e.target.classList.add('hover')
-    // console.log(e)
   }
   const handleMoverLeave=(e)=>{
+    // if(!isHover){
+    //   setIsHover(true)
+    // }
     setIsHover(false)
     e.target.classList.remove('hover')
+  }
+
+  //翻轉卡片
+  const handleToggleClick=(e)=>{
+    if(!isClick){
+      setIsClick(true)
+      e.target.classList.add('click')
+      return
+    }
+    setIsClick(false)
+    e.target.classList.remove('click')
   }
 
   return (
@@ -40,6 +65,7 @@ const CollectionCardItem = ({url})=>{
     <CardItemContainer
     onMouseEnter={(e)=>handleMouseOver(e)} 
     onMouseLeave={(e)=>handleMoverLeave(e)}
+    onClick={(e)=>handleToggleClick(e)}
     // style={{
     //   width:isHover?'200%':'100%',
     //   height:isHover? '200%':'100%',
